cfdf1bd4b6ef6ec8f7f334336fc7773df6fa87ee
David <kardav2019@gmail.com>
2024-02-09 15:49:27 +0200

    Modal

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,13 +1,16 @@
 import { BsPlusCircleFill } from 'react-icons/bs';
 import TaskCard from './TaskCard';
+import Modal from './Modal';
+import { useState } from 'react';
 
 const ProjectCard = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
     <div className="flex flex-col min-w-[32%] gap-4 px-3 py-2 border-dashed border-2 border-gray-300 rounded-lg">
       <header className="flex justify-between">
         <p className="font-bold">To do</p>
-        <div className="flex gap-1 items-center">
-          <BsPlusCircleFill />
+        <div className="flex gap-1 items-center cursor-pointer" onClick={() => setShowModal(true)}>
+          <BsPlusCircleFill/>
           <span className="font-semibold text-sm">Add new task</span>
         </div>
       </header>
@@ -17,6 +20,22 @@ const ProjectCard = () => {
         <TaskCard />
         <TaskCard />
       </div>
+      <Modal onClose={() => setShowModal(false)} show={showModal} header="New Task">
+        {/* Title */}
+        <input type='text' className='font-bold text-xl py-2 px-4 w-full border-gray-200 border rounded-lg' value="New Task"/>
+        {/* Description */}
+        <div className='flex flex-col gap-3'>
+          <label className='font-bold'>Description</label>
+          <input type='text' className='border-gray-200 border rounded-md py-2 px-4' placeholder='Small Description of the project'/>
+        </div>
+        <div className='flex flex-col gap-3'>
+          <label className='font-bold'>Description</label>
+          <textarea rows={4} className='border-gray-200 border rounded-md py-2 px-4' placeholder='Small Description of the project'/>
+        </div>
+        <button className='px-4 py-2 rounded-lg flex items-center gap-2 font-medium bg-primary justify-center' onClick={() => setShowModal(false)}>
+          Add Task
+        </button>
+      </Modal>
     </div>
   );
 };
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProjectCard from '../components/ProjectCard';
 import { useParams } from 'react-router-dom';
+import { createPortal } from 'react-dom';
+import Modal from '../components/Modal';
+
 
 const Project = () => {
   const params = useParams();
+  const [showModal, setShowModal] = useState<boolean>(false);
   const id = params.id;
 
   return (
-    <div className="flex overflow-x-scroll gap-3 h-full p-5 w-full">
+    <div className='flex flex-col gap-5 p-5'>
+      <button className='px-4 py-2 rounded-lg flex items-center gap-2 font-medium bg-primary      justify-center w-fit' onClick={() => setShowModal(true)}>
+        New Project
+      </button>
+    <div className="flex overflow-x-scroll gap-3 h-full overflow-hidden w-full">
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
+      <Modal onClose={() => setShowModal(false)} show={showModal} header="New Task">
+        {/* Title */}
+        <input type='text' className='font-bold text-xl py-2 pr-4 w-full' value="Update A-PROD"/>
+                {/* Description */}
+                <div className='flex flex-col gap-3'>
+                  <label className='font-bold'>Description</label>
+                  <textarea rows={4} className='border-gray-200 border rounded-md py-2 px-4' placeholder='Small Description of the project'/>
+                </div>
+                <button className='px-4 py-2 rounded-lg flex items-center gap-2 font-medium bg-primary justify-center' onClick={() => setShowModal(false)}>
+                  Submit
+                </button>
+      </Modal>
+      
+    </div>
     </div>
   );
 };
